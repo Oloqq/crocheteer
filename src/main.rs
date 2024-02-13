ea535cbca4b5ac8578e3242606c7f9f63f5f3491
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-02-13 05:31:03 +0100

    hot reload is fine

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,24 @@ use crate::meshes::*;
 
 mod meshes;
 
+use std::{thread, time};
+
 fn main() {
     use std::fs::OpenOptions;
 
     // let mesh = two_triangles();
     // let mesh = square();
-    let mesh = ring(6);
+    for i in 3..10 {
+        let mesh = ring(i);
 
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open("generated/test.stl")
-        .unwrap();
-    stl_io::write_stl(&mut file, mesh.iter()).unwrap();
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("generated/test.stl")
+            .unwrap();
+        stl_io::write_stl(&mut file, mesh.iter()).unwrap();
+
+        let period = time::Duration::from_millis(500);
+        thread::sleep(period);
+    }
 }
