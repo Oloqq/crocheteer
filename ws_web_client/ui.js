1c1e2e09bf07d943e6e38c1789b9cdde69cbd897
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-08-02 08:04:33 +0200

    hiding folders

---
+++ ws_web_client/ui.js
@@ -0,0 +1,14 @@
+const folders = document.getElementsByClassName("folder");
+for (let f of folders) {
+  const title = f.children[0];
+  const itemList = f.children[1];
+  title.addEventListener("click", () => {
+    const isHidden = itemList.getAttribute("hidden");
+    if (isHidden) {
+      itemList.removeAttribute("hidden");
+
+    } else {
+      itemList.setAttribute("hidden", true);
+    }
+  })
+}
