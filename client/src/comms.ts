ee95719179cb6c3e4a843268325f67b4d0cfcc98
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-09-05 08:15:32 +0200

    bring the dependencies

---
+++ client/src/comms.ts
@@ -0,0 +1,10 @@
+// let ws = undefined;
+
+export function send(text: string) {
+  console.log(`sending (not really): ${text}`);
+  // if (ws && ws.readyState === WebSocket.OPEN) {
+  //   ws.send(text);
+  // } else {
+  //   console.error("WebSocket is not open.");
+  // }
+}
