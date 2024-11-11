0b0566dea45198335df578d0f60ba1a9fd4b76e2
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-11-11 19:02:10 +0100

    handle visualize button

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,11 +2,21 @@ import { GuiData, initGui } from "./gui.ts";
 import * as render from "./render3d.ts";
 import World from "./World.ts";
 import * as leftPanel from "./leftPanel/panel.ts";
+import { send } from "./comms.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
   const editor = leftPanel.init();
   const display3d = render.init();
-  let guiData = new GuiData(editor.getValue, editor.setValue);
+  let guiData = new GuiData(
+    () => editor.getValue(),
+    (pattern: string) => editor.setValue(pattern)
+  );
   let world = new World("ws://127.0.0.1:8080", display3d, guiData);
   initGui(display3d, guiData, world);
+
+  const visualizeButton = document.getElementById("visualize-button");
+  visualizeButton?.addEventListener("click", () => {
+    const pattern = guiData.getPattern();
+    send(`pattern flow ${pattern}`);
+  });
 });
