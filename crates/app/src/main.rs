13aea25c7618eff7b7809541caba81d858b9dce8
Olgierd Piofczyk <olgierdpk@gmail.com>
2026-03-19 00:53:43 +0100

    initial

---
+++ crates/app/src/main.rs
@@ -0,0 +1,13 @@
+use crochetlib::add;
+
+use bevy::prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Add 2 + 3 = {}", add(2, 3));
+
+    let x = Vec3::ZERO;
+    let y = crochetlib::v0();
+    let z = x + y;
+    println!("compatible types yay {}", z);
+}
