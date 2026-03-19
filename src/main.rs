0a914b0101cbe9db4bb2fadb9becaf68e4de9059
Olgierd Piofczyk <olgierdpk@gmail.com>
2026-03-19 19:03:38 +0100

    structure

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use crochet::add;
+
+use bevy::prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Add 2 + 3 = {}", add(2, 3));
+
+    let x = Vec3::ZERO;
+    let y = crochet::v0();
+    let z = x + y;
+    println!("compatible types yay {}", z);
+}
